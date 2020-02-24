6578ac1a6918968e7d5485aae93d30779efaeaed
AliciaMerlino <acmerlino1@gmail.com>
2020-02-24 11:37:34 +0000

    balance starts at 0

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -5,5 +5,10 @@ describe BankAccount do
   it "starts with a balance of 0" do
     expect(subject.balance).to eq(0)
   end
+
+  describe "Deposit of 10.00 increases baance to 10.00" do
+    subject.deposit(10.00)
+    expect(subject.balance).to eq(10.00)
+  end
   
 end
