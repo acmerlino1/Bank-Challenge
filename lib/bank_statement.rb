7a57b4ba918e0030722d58f0f7ed9698e535497e
AliciaMerlino <acmerlino1@gmail.com>
2020-02-24 12:58:27 +0000

    Statement has no transaction history to begin

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -1,2 +1,8 @@
 class BankStatement
+
+attr_reader :transactions
+
+  def initialize
+    @transactions = []
+  end
 end
--- spec/bank_statement_spec.rb
+++ spec/bank_statement_spec.rb
@@ -1,5 +1,9 @@
 require 'bank_statement'
 
 describe BankStatement do
+
+  it 'has a blank transaction history when initialized' do
+    expect(subject.transactions).to be_empty
+  end
   
 end
