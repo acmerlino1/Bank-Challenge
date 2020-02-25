648e1424a83908ca09b20c624b0e890c72b941dd
AliciaMerlino <acmerlino1@gmail.com>
2020-02-25 14:41:25 +0000

    Make time_now local variable rather than instance variable

--- spec/bank_statement_spec.rb
+++ spec/bank_statement_spec.rb
@@ -18,7 +18,7 @@ describe BankStatement do
       time_now = Time.now.strftime("%d/%m/%Y")
       allow(Time).to receive(:now).and_return(time_now)
       subject.add_to_history(@transaction)
-      expect(subject.transaction_history).to include({time: @time_now, credit: 100.00, debit: nil, balance: 100.00}) 
+      expect(subject.transaction_history).to include({time: time_now, credit: 100.00, debit: nil, balance: 100.00}) 
     end
   end
 
