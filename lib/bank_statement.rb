ec7d43e4a024a5e7a25d65fb9f04580ae27680ad
AliciaMerlino <acmerlino1@gmail.com>
2020-02-29 18:24:18 +0000

    Statement class now adds transaction so to statement, does not do any printing or formatting

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -10,15 +10,5 @@ class BankStatement
   def add_to_history(transaction)
     @transaction_history << transaction
   end
-
-  def print_statement
-    statement = ['date || credit || debit || balance']
-    @transaction_history.reverse.each do |transaction|
-      single_transaction = "#{transaction[:time]} ||"\
-      "  #{transaction[:credit]} || #{transaction[:debit]}"\
-      " || #{transaction[:balance]}"
-      statement.push(single_transaction)
-    end
-    puts statement
-  end
+  
 end
--- lib/display.rb
+++ lib/display.rb
@@ -1,2 +1,12 @@
 class Display
+
+  # statement = ['date || credit || debit || balance']
+  #   @transaction_history.reverse.each do |transaction|
+  #     single_transaction = "#{transaction[:time]} ||"\
+  #     "  #{transaction[:credit]} || #{transaction[:debit]}"\
+  #     " || #{transaction[:balance]}"
+  #     statement.push(single_transaction)
+  #   end
+  #   puts statement
+
 end
